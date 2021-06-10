da3ec65040a6602d774856beea81dda53292252b
Sarah Hamed <sarahhamed2496@gmail.com>
2021-06-10 05:30:41 -0700

    api show content assign

---
+++ showAPI/playground.js
@@ -0,0 +1,168 @@
+//clousers
+// const myClouser = () =>{
+//     return{
+//         hello(){console.log('hello')},
+//         bye(){console.log('bye')},
+//         goodEvening(){console.log('good evening')}
+//     }
+// }
+// console.log(myClouser().hello())
+// const myClouser  = (val) =>{
+//     return {
+//         val,
+//         vale(){return val},
+//         add(x){return val+=x},
+//         sub(x) {return  val-=x}
+//     }
+// }
+// const myClouser1  = function(val) {
+//     return {
+//         val,
+//         vale(){return this.val},
+//         add(x){return this.val+=x},
+//         sub(x) {return  this.val-=x}
+//     }
+// }
+// let myVal = myClouser1(5)
+// console.log(myVal.sub(6))
+// console.log(myVal.vale())
+// console.log(myVal.add(50))
+// console.log(myVal.val)
+
+//callback
+
+// setTimeout(()=> { console.log('hi') } , 2000)
+//  const myCB = (num ,cb )=>{
+//     if(num<5) cb('done', false)  //=> (res, error) 
+//     else cb(false, 'fe error')
+//  }
+
+//  myCB(4, (data, err)=>{
+//           if(!err) console.log(data)
+//           else console.log(err)
+//  })
+/* number => odd even  even only */
+
+// const testNumber = (num, myReturn) =>{  //err=>false, "err"  data=>"tmam", false
+//     if(num%2==0) myReturn(false, "tamam") // return {err:false, data:"tmam"}
+//     else myReturn('Err', false)
+// }
+//('err', false)  (false, 'tmam')
+// testNumber(9, (e,d)=>{
+//     if(e) console.log(e)
+//     else console.log(d);
+// })
+
+//promises
+// setTimeout(()=>console.log('a'), 2000)
+// setTimeout(()=>{console.log('b')}, 2000)
+// console.log('c')
+// const myPromise = (num) =>{
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             typeof num=="number"? resolve('done'): reject('error in data')
+//         },2000)
+//     })
+// }
+// try{
+    // myAsyncFun()
+// }
+// catch(e){
+// console.log(e.message)
+// }
+//async await
+// x= await myPromise(5)
+// console.log(x)
+// const myAsyncFun = async()=>{
+//     try{
+//         x=await(myPromise("hello"))
+//         console.log(x)
+    
+//     }
+//     catch(e){
+// console.log(e)
+//     }
+// }
+// myAsyncFun()
+
+//data=[1,2,3,4,5]  invalid credin
+// const myCred = (userName)=>{
+//     let p = new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             if(userName=="marwa"|| userName=="mohammed"){
+//                 resolve([1,2,3,4,5])
+//             }
+//             else {
+//                 reject('unauthorized')
+//             }
+//         }, 1500)
+//     })
+//     return p
+// }
+// handelData = async()=>{
+//     try{
+//         d = await myCred('marwa')
+/*
+d=await myCre(0)
+x= await myCre()
+z= await myCred()
+ */
+//         console.log(d)
+//     }
+//     catch(e){
+//         console.log(e)
+//     }
+// }
+// handelData()
+//then catch
+// myCred('ahmed').then(
+//     (res)=>{
+//         console.log(res)
+//     }
+// ).catch(
+//     (e)=>console.log(e)
+// )
+//apiCall
+
+
+
+const getData = async(callback) =>{
+    try{
+    let x = await fetch('https://api.covid19api.com/summary')
+    let y = await(x.json())
+   //  console.log(y['Countries'])   
+    // return y
+    callback(y.Countries, false)
+    }
+    catch(e){
+        callback(false, e)
+    }
+}
+// let myData = getData()
+// console.log(myData)
+elements=["ID","Country","CountryCode","Slug","NewConfirmed","TotalConfirmed","NewDeaths","TotalDeaths","NewRecovered","TotalRecovered","Date"]
+getData( (data, error)=> {
+    if(data){//console.log(data)
+
+    data.forEach((elem,i)=>{
+       // console.log(elem.Country)
+        document.querySelector('.row').innerHTML=document.querySelector('.row').innerHTML+`<div class="col-md-4"><div class="m-2  bg-dark text-secondary p-4 text-center"></div></div>`
+        elements.forEach(el=>{
+            document.querySelectorAll('.m-2')[i].innerHTML  = document.querySelectorAll('.m-2')[i].innerHTML+`<p>`+el+`: `+elem[el]+`</p>`;
+           console.log(elem[el])
+        })
+       // document.querySelector('.row').innerHTML=document.querySelector('.row').innerHTML+`</div>`
+       // console.log(document.querySelector('.row').innerHTML)
+        
+    })
+    }
+    else console.log(error)
+})
+
+
+
+
+
+
+
+
