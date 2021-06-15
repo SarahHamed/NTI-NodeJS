d75eb45d5e75253a723d8543a14116301f40ce08
SarahHamed <sarahhamed2496@gmail.com>
2021-06-14 18:23:59 -0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-/*const express = require('express')
-app=express()*/
-const app = require('./src/app')
-const PORT = 3500
-app.listen(PORT, ()=>{
-    console.log(`we are live on localhost:${PORT}`)
-})
